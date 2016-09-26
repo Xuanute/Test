49adb910d2e72bda52270469e8376fdad57f25c1
Xuanute <xuantrinh812@gmail.com>
2016-09-26 21:47:47 +0700

    Demo commit

---
+++ DemoGithub/src/demo/Data.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Data {
+	int data[] = {0, 12, 45};
+	
+	public void display(){
+		System.out.println("To do implement data display");
+	}
+
+}
---
+++ DemoGithub/src/demo/Main.java
@@ -0,0 +1,21 @@
+package demo;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Data data = new Data();
+		displayScreen();
+		data.display();
+	}
+	public static void displayScreen()
+	{
+		System.out.println("hello 1");
+		System.out.println("Hello 2");
+		System.out.println("Hello 3");
+		System.out.println("afbvvvf");
+		System.out.println("new test");
+		
+	}
+
+}
